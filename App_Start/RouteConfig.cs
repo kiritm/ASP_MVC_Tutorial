eda04837c854bdd54934551c4a2e43dd290fcaa1
Kirit Maganlal Shah <kiritshah2@yahoo.com.sg>
2018-12-29 23:44:48 +0800

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RoutingExample
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "MyHome",
+               url: "MyHome",
+               defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "MyHome1",
+               url: "",
+               defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "MyHome2",
+               url: "MyHome/MyHome",
+               defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "MyHome", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
